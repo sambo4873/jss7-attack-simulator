57eda5279a3f6246933965e5c9ce5ba7a28db9c6
Kristoffer Jensen <jensenkris91@gmail.com>
2016-02-04 14:20:42 +0100

    Add new attack-file

---
+++ tools/simulator/core/src/main/java/org/mobicents/protocols/ss7/tools/simulator/tests/attack/TestSRIForSMMan.java
@@ -0,0 +1,99 @@
+package org.mobicents.protocols.ss7.tools.simulator.tests.attack;
+
+import org.mobicents.protocols.ss7.map.api.MAPDialogListener;
+import org.mobicents.protocols.ss7.map.api.service.sms.*;
+import org.mobicents.protocols.ss7.tools.simulator.common.TesterBase;
+
+public class TestSRIForSMMan extends TesterBase implements MAPDialogListener,
+        MAPServiceSmsListener {
+
+    public static String SOURCE_NAME = "TestSRIForSMClient";
+    private final String name;
+
+    private boolean isStarted = false;
+
+    public TestSRIForSMMan(String name) {
+        super(SOURCE_NAME);
+        this.name = name;
+    }
+
+    @Override
+    public void onForwardShortMessageRequest(ForwardShortMessageRequest forwSmInd) {
+
+    }
+
+    @Override
+    public void onForwardShortMessageResponse(ForwardShortMessageResponse forwSmRespInd) {
+
+    }
+
+    @Override
+    public void onMoForwardShortMessageRequest(MoForwardShortMessageRequest moForwSmInd) {
+
+    }
+
+    @Override
+    public void onMoForwardShortMessageResponse(MoForwardShortMessageResponse moForwSmRespInd) {
+
+    }
+
+    @Override
+    public void onMtForwardShortMessageRequest(MtForwardShortMessageRequest mtForwSmInd) {
+
+    }
+
+    @Override
+    public void onMtForwardShortMessageResponse(MtForwardShortMessageResponse mtForwSmRespInd) {
+
+    }
+
+    @Override
+    public void onSendRoutingInfoForSMRequest(SendRoutingInfoForSMRequest sendRoutingInfoForSMInd) {
+
+    }
+
+    @Override
+    public void onSendRoutingInfoForSMResponse(SendRoutingInfoForSMResponse sendRoutingInfoForSMRespInd) {
+
+    }
+
+    @Override
+    public void onReportSMDeliveryStatusRequest(ReportSMDeliveryStatusRequest reportSMDeliveryStatusInd) {
+
+    }
+
+    @Override
+    public void onReportSMDeliveryStatusResponse(ReportSMDeliveryStatusResponse reportSMDeliveryStatusRespInd) {
+
+    }
+
+    @Override
+    public void onInformServiceCentreRequest(InformServiceCentreRequest informServiceCentreInd) {
+
+    }
+
+    @Override
+    public void onAlertServiceCentreRequest(AlertServiceCentreRequest alertServiceCentreInd) {
+
+    }
+
+    @Override
+    public void onAlertServiceCentreResponse(AlertServiceCentreResponse alertServiceCentreInd) {
+
+    }
+
+    @Override
+    public void onReadyForSMRequest(ReadyForSMRequest request) {
+
+    }
+
+    @Override
+    public void onReadyForSMResponse(ReadyForSMResponse response) {
+
+    }
+
+    @Override
+    public void onNoteSubscriberPresentRequest(NoteSubscriberPresentRequest request) {
+
+    }
+}
