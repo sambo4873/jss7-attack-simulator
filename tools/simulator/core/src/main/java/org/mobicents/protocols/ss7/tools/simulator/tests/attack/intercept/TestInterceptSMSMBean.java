9f57e85df1ba76d3b57bb3472a7757920825cba6
Kristoffer Jensen <jensenkris91@gmail.com>
2016-02-11 12:38:52 +0100

    Change author

--- tools/simulator/core/src/main/java/org/mobicents/protocols/ss7/tools/simulator/tests/attack/intercept/TestInterceptSMSMBean.java
+++ tools/simulator/core/src/main/java/org/mobicents/protocols/ss7/tools/simulator/tests/attack/intercept/TestInterceptSMSMBean.java
@@ -1,7 +1,7 @@
 package org.mobicents.protocols.ss7.tools.simulator.tests.attack.intercept;
 
 /**
- * Created by kristoffer on 11.02.2016.
+ * @author Kristoffer Jensen
  */
 public class TestInterceptSMSMBean {
 }
