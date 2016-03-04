95b6b1467fafe830122537c595e3df97ae3f046c
Kristoffer Jensen <jensenkris91@gmail.com>
2016-03-04 08:52:51 +0100

    Checking input

--- tools/simulator/bootstrap/src/main/java/org/mobicents/protocols/ss7/tools/simulator/bootstrap/Main.java
+++ tools/simulator/bootstrap/src/main/java/org/mobicents/protocols/ss7/tools/simulator/bootstrap/Main.java
@@ -164,6 +164,7 @@ public class Main {
                 case 's':
                     //Number of subscribers to generate in complex attack simulation.
                     arg = g.getOptarg();
+                    System.out.println("Passed s value: " + arg);
                     this.complexNumSubs = Integer.valueOf(arg);
                     break;
                 case 1:
