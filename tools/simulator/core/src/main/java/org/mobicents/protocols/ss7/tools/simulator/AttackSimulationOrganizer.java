3a759c5a36f2affe06f9386bd0d6ebfa80b42994
Kristoffer Jensen <jensenkris91@gmail.com>
2016-03-14 10:16:15 +0100

    Speed up simulation

--- tools/simulator/core/src/main/java/org/mobicents/protocols/ss7/tools/simulator/AttackSimulationOrganizer.java
+++ tools/simulator/core/src/main/java/org/mobicents/protocols/ss7/tools/simulator/AttackSimulationOrganizer.java
@@ -942,7 +942,7 @@ public class AttackSimulationOrganizer implements Stoppable {
         while (true) {
             try {
                 sleepTime = this.random.nextInt((1000 - 100) + 1) + 100;
-                sleepTime = 500;
+                sleepTime = 100;
                 Thread.sleep(sleepTime);
 
                 if (this.testerHostsNeedQuit())
