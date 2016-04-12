bfa928e0ca0e54a5cf6bfdf33a5daa38f7e713b4
Kristoffer Jensen <jensenkris91@gmail.com>
2016-04-12 17:28:10 +0200

    Change mercy time and max run time for simulator, ~38 hours to complete

--- tools/simulator/core/src/main/java/org/mobicents/protocols/ss7/tools/simulator/AttackSimulationOrganizer.java
+++ tools/simulator/core/src/main/java/org/mobicents/protocols/ss7/tools/simulator/AttackSimulationOrganizer.java
@@ -1000,8 +1000,8 @@ public class AttackSimulationOrganizer implements Stoppable {
                 currentRuns = 0,
                 hour_count = 100,
                 week_count = hour_count * 24 * 7,
-                warmUpRuns = week_count * 3, // 168000 counts = one week
-                maxRuns = week_count * 4,
+                warmUpRuns = week_count * 6, // 168000 counts = one week
+                maxRuns = week_count * 8,
                 move_a_1 = (int)(hour_count * 8.1),
                 move_a_2 = (int)(hour_count * 8.2),
                 move_a_3 = (int)(hour_count * 8.3),
